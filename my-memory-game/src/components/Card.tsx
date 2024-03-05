210b8294c14192592000c9f3a2466e35fbd14c94
dracc <ssrinet10@gmail.com>
2024-03-06 02:14:35 +0530

    added ui components

--- my-memory-game/src/components/Card.tsx
+++ my-memory-game/src/components/Card.tsx
@@ -11,7 +11,7 @@ export const Card: React.FC<CardProps>=({img,name})=>{
 
     return(
         <>
-        <div className="card bg-blue-200 relative border-spacing-0 p-0 opacity-60 h-40 w-30 flex flex-col justify-center items-center">
+        <div className="card bg-yellow-300  relative border-spacing-0 p-0 opacity-90 h-40 w-30 flex flex-col justify-center items-center">
          <img src={img} alt={name} className="max-h-full max-w-full border-0"></img>
          <div className="name font-sans font-bold">{name}</div>
         </div>
